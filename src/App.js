d53c4bb54dd6cb6a6ba307bf3a373ab7870404e7
nv-gordeev <nv-gordeev@ya.ru>
2018-02-06 23:42:44 +0500

    added jquery (for bootstrap)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './styles/App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Система психологического тестирования</h1>
         </header>
         <main className="AppBody">
-            {this.props.children}
+            <div className="container">
+                {this.props.children}
+            </div>
         </main>
       </div>
     );
