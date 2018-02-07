41b17c82a1225f996f7ba60f91fa025700b61a91
nv-gordeev <nv-gordeev@ya.ru>
2018-02-07 22:55:41 +0500

    added reducer factory and action creator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import {combineReducers} from "redux";
+import {apiReducerFactory} from "./apiReducerFactory";
 
-export default combineReducers({})
+export default combineReducers({
+    questions: apiReducerFactory('question'),
+    results: apiReducerFactory('result'),
+    persons: apiReducerFactory('person'),
+})
