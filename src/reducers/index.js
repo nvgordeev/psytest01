c3f932e4c8c22460abc7e310d0263f0f45860c61
nv-gordeev <nv-gordeev@ya.ru>
2018-02-08 22:32:04 +0500

    added testingReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {apiReducerFactory} from "./apiReducerFactory";
+import {testingReducer} from "./testing";
 
 export default combineReducers({
     questions: apiReducerFactory('question'),
     results: apiReducerFactory('result'),
     persons: apiReducerFactory('person'),
+    testing: testingReducer
 })
---
+++ src/reducers/testing.js
@@ -0,0 +1,22 @@
+import * as ACTION from '../constants/actions'
+
+const initialState = {
+    personId: null,
+    questions: [],
+    result: null,
+    complete: false
+}
+
+export function testingReducer(state=initialState, action) {
+    const {type, data} = action
+    switch (type) {
+        case ACTION.TEST_PROCESS_START:
+            return {...initialState, personId: data.personId}
+        case ACTION.TEST_PROCESS_ANSWER:
+            return {...state, questions: [...state.questions, data.question]}
+        case ACTION.TEST_PROCESS_FINISH:
+            return {...state, result: data.result}
+        default:
+            return state
+    }
+}
